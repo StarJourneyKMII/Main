9150edaa9372329df62dc8f5ff593275ea159892
man572142 <87473853+man572142@users.noreply.github.com>
2022-11-13 19:04:51 +0800

    add door animation

--- Assets/_STU_Project/Scripts/Level/Trap/DoorNext.cs
+++ Assets/_STU_Project/Scripts/Level/Trap/DoorNext.cs
@@ -2,19 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class DoorNext : MonoBehaviour
 {
     //private string nextLevelName = "STJ_Old_Level";
     //private string playName = "STJ_PLAY1";
     [SerializeField] private EvaluationForm winPanel;
+    [SerializeField] private Animator animator;
+    private bool isTrigger;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !isTrigger)
         {
-            winPanel.Show();
+            //winPanel.Show();
+            isTrigger = true;
+            animator.SetTrigger("OpenDoor");
+            StartCoroutine(PlayAnimation(collision.transform.gameObject));
         }
     }
+
+    private IEnumerator PlayAnimation(GameObject player)
+    {
+        yield return new WaitForSeconds(0.8f);
+        player.SetActive(false);
+
+        yield return new WaitForSeconds(2f);
+        winPanel.Show();
+    }
+
 
     public void GotoNextLevel()
     {
